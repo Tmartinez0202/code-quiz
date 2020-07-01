641e43dc0ef0134ab862a01464997df674252e00
OkieKenobi <travismartinez88@gmail.comgit config --global user.name Tmartinez0202>
2020-07-01 17:19:26 -0600

    added questions for the quiz

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,17 +2,35 @@ var questions = [
 
 
     {
-        title: "first title",
-        choices: ["a", "1", "b", "c"],
-        answer: "1"
+        title: "What is computer coding?",
+        choices: ["Telling a computer what to do", "A list of functions", "A radio show", "A tv show"],
+        answer: "Telling a computer what to do"
 
     },{
 
-        title: "second title",
-        choices: ["a", "b", "3", "d"],
-        answer: "3"
+        title: "What word describes the set of instructions that computers need to do work?",
+        choices: ["Synopsis", "Program", "Agenda", "Blueprint"],
+        answer: "Program"
 
-    },//at least 4 questions
+    },{
+
+        title: "What are people who write computer code called?",
+        choices:["Cryptographers", "Professors", "Programmers", "Manufacturers"],
+        answer:"Programmers",
+
+    },{
+
+        title:"Which of these is NOT a programming language?",
+        choices:["Banana", "Ruby", "Java", "Python"],
+        answer:"Banana",
+
+    },{
+
+        title:"Which of these is a programming language?",
+        choices:["Itch", "Scratch", "Gnaw", "Bite"],
+        answer:"Scratch",
+
+    }
 
 
 
